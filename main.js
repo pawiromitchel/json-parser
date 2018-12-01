806de6f9c6cc50c0ec9ccadb7611f6462c2ff73f
Mitchel Pawirodinomo <pawiromitchel@hotmail.com>
2018-12-01 16:12:16 -0300

    first release, doesnt handle nested objects yet

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let dataValues;
+let authIsToggled = false;
+const authBlock = document.getElementById('authBlock');
+authBlock.style.display = 'none';
 
 const request = (url, method, authKey = ``, body = ``, callback) => {
     const fetchHeaders = new Headers();
@@ -22,30 +25,59 @@ const request = (url, method, authKey = ``, body = ``, callback) => {
         .catch(error => console.error(error))
 }
 
+const requestNoToken = (url, callback) => {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        callback(data);
+    })
+    .catch(error => console.error(error))
+}
+
 function doLogin() {
-    const loginUrl = document.getElementById("loginUrl").value;
-    const username = document.getElementById("loginUsername").value;
-    const password = document.getElementById("loginPassword").value;
+    const loginUrl = document.getElementById('loginUrl').value;
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
 
     request(loginUrl, `POST`, '', {
-        "username": username,
-        "password": password
+        'username': username,
+        'password': password
     }, (res) => {
         if (res.token) {
             const token = res.token;
-            localStorage.setItem("token", token);
+            localStorage.setItem('token', token);
         } else {
-            alert("Not authenticated");
+            alert('Not authenticated');
         }
     });
 }
 
+function toggleAuth() {
+    if (document.getElementById('toggleAuth').checked) {
+        authIsToggled = false;
+        authBlock.style.display = 'none';
+    } else {
+        authIsToggled = true;
+        authBlock.style.display = 'block';
+    }
+}
+
 function doRequestData() {
-    const token = localStorage.getItem("token");
-    const requestUrl = document.getElementById("requestUrl").value;
-    request(requestUrl, `GET`, token, '', (users) => {
-        dataValues = users;
-    });
+
+    const requestUrl = document.getElementById('requestUrl').value;
+
+    if (authIsToggled) {
+        const token = localStorage.getItem('token');
+        request(requestUrl, `GET`, token, '', (data) => {
+            dataValues = data;
+            doGenerateViewer();
+        });
+    } else {
+        requestNoToken(requestUrl, (data) => {
+            dataValues = data;
+            doGenerateViewer();
+        });
+    }
 }
 
 function doGenerateViewer() {
@@ -57,13 +89,13 @@ function doGenerateViewer() {
     createViewer(`viewer-table`, dataValues, columns);
 }
 
-
 function createViewer(elementID, tabledata, columns) {
-    //create Tabulator on DOM element with id "example-table"
+    //create Tabulator on DOM element with id 'example-table'
     var table = new Tabulator(`#${elementID}`, {
-        height: 205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
         data: tabledata, //assign data to table
-        layout: "fitColumns", //fit columns to width of table (optional)
-        columns: columns
+        layout: 'fitDataFill', //fit columns to width of table (optional)
+        columns: columns,
+        pagination:'local',
+        paginationSize:15,
     });
 }
